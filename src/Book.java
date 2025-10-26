f828da0fb024a2b68fa1f199864bec44960fe86f
Daniel Keith <danielrkeith@mac.lan>
2025-10-26 23:07:08 +1100

    Implemented minMaxVersesPerDay for Book

--- src/Book.java
+++ src/Book.java
@@ -20,6 +20,53 @@ public class Book {
         return verses;
     }
 
+    public int minMaxVersesPerDay(int days) {
+        // Find a possible and impossible maxVersesPerDay
+        int possibleMaxVersesPerDay = 1;
+
+        while (daysToMemorise(possibleMaxVersesPerDay) > days) {
+            possibleMaxVersesPerDay *= 2;
+        }
+
+        int impossibleMaxVersesPerDay = possibleMaxVersesPerDay / 2;
+
+        // Converge possibleMaxVersesPerDay and impossibleMaxVersesPerDay to be one apart
+        while (possibleMaxVersesPerDay > impossibleMaxVersesPerDay + 1) {
+            int middleMaxVersesPerDay = (possibleMaxVersesPerDay + impossibleMaxVersesPerDay) / 2;
+
+            if (daysToMemorise(middleMaxVersesPerDay) > days) {
+                impossibleMaxVersesPerDay = middleMaxVersesPerDay;
+            } else {
+                possibleMaxVersesPerDay = middleMaxVersesPerDay;
+            }
+        }
+
+        return possibleMaxVersesPerDay;
+    }
+
+    private int daysToMemorise(int maxVersesPerDay) {
+        int day = 1;
+        int versesInDay = 0;
+
+        for (Chapter chapter : chapters) {
+            if (chapter.getVerses() > maxVersesPerDay) {
+                return Integer.MAX_VALUE;
+            }
+
+            // Can the chapter be memorised today?
+            if (versesInDay + chapter.getVerses() <= maxVersesPerDay) {
+                // Memorise it today
+                versesInDay += chapter.getVerses();
+            } else {
+                // Memorise it tomorrow
+                day++;
+                versesInDay = chapter.getVerses();
+            }
+        }
+
+        return day;
+    }
+
     @Override
     public String toString() {
         return book + " (" + getVerses() + " verses)";
